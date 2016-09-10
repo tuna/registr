8359c122fd657893b6ede9aeb4a289046fbf7ce7
Xavier Yao <xavieryao@live.cn>
2016-09-10 21:04:55 +0800

    remove unique constraint of stu_number

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ class Candidate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
     department = db.Column(db.String(128), nullable=False)
-    stu_number = db.Column(db.String(16), unique=True)
+    stu_number = db.Column(db.String(16), unique=False)
     phone = db.Column(db.String(16), nullable=False)
     email = db.Column(db.String(120), unique=True)
     gender = db.Column(db.Enum('男', '女'))
